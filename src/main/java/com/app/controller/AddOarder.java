5bd737bb9e51320e10b6cf28beff18bc2a5338b2
Sandeep Raj <sabakhanjmi0@gmail.com>
2024-02-17 11:42:08 +0530

    new changes

--- src/main/java/com/app/controller/AddOarder.java
+++ src/main/java/com/app/controller/AddOarder.java
@@ -17,7 +17,7 @@ public class AddOarder extends HttpServlet{
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("uvbifdvbitbitb");
+			System.out.println("This is a new commit to check rules");
 		}
 		resp.getWriter().println("Done");
 	}
